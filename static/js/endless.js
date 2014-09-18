24b0f29f3fa86fbb0136c91b94fae1055b812224
Tal Ater <tal@talater.com>
2014-09-18 22:04:19 +0300

    Implemented handheld interface

--- static/js/endless.js
+++ static/js/endless.js
@@ -2,10 +2,31 @@
   var _name = 'incognito';
   var signin = function() {
     _name = $('#signin-input')[0].value || _name;
-    window.console.log(_name);
+
+    $('#signin-container').animate({'height': '0%'}, 500);
+    $('#control-container').animate({'height': '100%'}, 500);
+    $('#left-button, #right-button').show();
+
     return false;
   };
 
   $('#signin-form button').on('click', signin);
   $('#signin-form').on('submit', signin);
+
+  $('#left-button, #right-button').on('click', function() {
+    var direction = $(this).data('direction');
+    $.ajax({
+        type: 'GET',
+        url: '/vote',
+        data: {"side": direction, "name": _name},
+        success: function() {},
+        error: function() {},
+        cache:false
+    });
+
+  });
+
+  // Preloading like is 1999
+  var imagePhone = new Image();
+  imagePhone = '/static/img/keypad.jpg';
 })();
