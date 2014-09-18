af3b69de0a2d92b6919a87925e0e7fe87db7531a
Jonathan Livni <jl@consumerphysics.com>
2014-09-18 23:59:40 +0300

    save context in redis, added reset api, status now returns a lot of info, movement calculated on status instead of on vote

---
+++ redis_conduit.py
@@ -0,0 +1,12 @@
+import redis_wrap
+
+redis_url = 'redis://redistogo:1a64b60564984583a8a0aad77eb232b4@jack.redistogo.com:9604/'
+# redis_url = 'redis://127.0.0.1:6379/'
+redis_wrap.from_url(redis_url)
+
+
+left = redis_wrap.get_list('left')
+right = redis_wrap.get_list('right')
+color_mapping = redis_wrap.get_hash('color-mapping')
+vote_count = redis_wrap.get_hash('vote-count')
+state = redis_wrap.get_hash('state')  # assorted vars such as current-position
